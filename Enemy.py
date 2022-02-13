8a031278d291828bd983e3b8e399918e94f6aff5
Haiphicoo <haikal.cooper@vikings.berry.edu>
2022-02-13 16:12:02 -0500

    Ease
    Added easing function

--- Enemy.py
+++ Enemy.py
@@ -1,4 +1,5 @@
 import pygame
+from Player import Player
 
 class Enemy:
 
@@ -7,6 +8,7 @@ class Enemy:
     height = 100 
     speed = 10
     isDead = False
+    easing = .05
 
 
 # health, width, height, speed, x, y, isDead
@@ -21,3 +23,9 @@ class Enemy:
 
         ## add easing function 
         ## enemies will ease towards the heroes(players)
+    def ease(self, aPlayerX, aPlayerY):
+        dx = aPlayerX - self.x
+        self.x += dx * self.easing
+
+        dy = aPlayerY - self.y
+        self.y += dy * self.easing
--- main.py
+++ main.py
@@ -55,6 +55,7 @@ def main():
         elif keysPressed[pygame.K_a]== True:
             player1.x-= player1.speed
 
+        enemy1.ease(player1.x, player1.y)
 
         # put code here that should be run every frame
          # of your game             
