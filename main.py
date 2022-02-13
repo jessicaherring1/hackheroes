d46a2d7ea9b40fa139bcace2850f53e134b40c0b
jessicaherring1 <jessica.herring123@gmail.com>
2022-02-13 17:22:15 -0500

    current

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import pygame
 from Player import Player
 from Enemy import Enemy 
+from Button import Button
+
 
 # Define the size of the game window
 WIDTH = 800
 HEIGHT = 600
-# make the game window object
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-# name the game window
-pygame.display.set_caption("Hackheroes")
 
-FPS=60
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT)) # make the game window object
+
+
+pygame.display.set_caption("Hackheroes") # name the game window
+
+FPS=60 # frames per second
 
 player1= Player(200, 100)
 enemy1 = Enemy(500, 500)
+button1 = Button( 50, 50, 50, 50, (255, 0, 0), "hello")
+
+state = 1
 
 # yellowShipImg = pygame.image.load("Assets/spaceship_yellow.png")
 # redShipImg = pygame.image.load("Assets/spaceship_red.png")
@@ -21,51 +27,64 @@ enemy1 = Enemy(500, 500)
 def main():
     clock = pygame.time.Clock()
 
-# make a boolean that represents whether the game should continue to run or not
-    running = True
-
+    running = True #boolean that represents whether the game should continue to run or not
 
     # while the game is running
     while running:
         WINDOW.fill((0,0,0))
-        # this makes it so this function can run at most FPS times/sec
-        clock.tick(FPS)
+        
+        clock.tick(FPS) # this makes it so this function can run at most FPS times/sec
 
         # for all the game events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                running = False
                pass
-
-        player1.render(WINDOW)
-        enemy1.render(WINDOW)
-
-        enemy1.resetEnemyBoundaries
-        player1.resetBoundaries
-
-        # this gets a list of booleans showing which keys are currently pressed
-        keysPressed = pygame.key.get_pressed()
-
-        if keysPressed[pygame.K_w]== True:
-            player1.y-=player1.speed
-
-        if keysPressed[pygame.K_s]== True:
-            player1.y+= player1.speed
         
-        if keysPressed[pygame.K_d]== True:
-            player1.x+= player1.speed
+        if state ==0: # home screen
+            pass
 
-        if keysPressed[pygame.K_a]== True:
-            player1.x-= player1.speed
+        if state == 1: #game state
 
-        enemy1.ease(player1.x, player1.y)
-        enemy1.enemyHit(player1)
+            player1.render(WINDOW)
+            player1.move()
+            player1.resetBoundaries()
+            enemy1.render(WINDOW)
+            enemy1.resetEnemyBoundaries()
 
-    
+            button1.render(WINDOW)
 
-        #pygame.Rect.colliderect(player1, enemy1)
+            # this gets a list of booleans showing which keys are currently pressed
+            keysPressed = pygame.key.get_pressed()
 
-        # put code here that should be run every frame
-         # of your game             
-        pygame.display.update()
+            if keysPressed[pygame.K_w]== True:
+                player1.movingUp = True
+
+            if keysPressed[pygame.K_s]== True:
+                player1.movingDown = True
+            
+            if keysPressed[pygame.K_d]== True:
+                player1.movingRight = True
+
+            if keysPressed[pygame.K_a]== True:
+                player1.movingLeft = True
+
+            if keysPressed[pygame.K_w]== False:
+                player1.movingUp = False
+
+            if keysPressed[pygame.K_s]== False:
+                player1.movingDown = False
+            
+            if keysPressed[pygame.K_d]== False:
+                player1.movingRight = False
+
+            if keysPressed[pygame.K_a]== False:
+                player1.movingLeft = False
+
+            enemy1.ease(player1.x, player1.y)
+            enemy1.enemyHit(player1)
+
+            #pygame.Rect.colliderect(player1, enemy1)
+                       
+            pygame.display.update()
 main()
