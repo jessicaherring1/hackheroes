eb4f5eab389483b2641ad557af7e49f313ae0212
Haiphicoo <haikal.cooper@vikings.berry.edu>
2022-02-17 14:14:38 -0500

    created Animation class

---
+++ Animation.py
@@ -0,0 +1,39 @@
+import pygame
+from sqlalchemy import false
+
+
+class Animation:
+    # variables
+    index = 0
+    isAnimating = false
+
+    def __init__(self, tempImages, tempSpeed, tempScale):
+        self.Images = tempImages
+        self.speed = tempSpeed
+        self.scale = tempScale
+
+  # updates the index which image to display for
+  # the animation
+
+    def next(self):
+        print(self.index)
+        self.index += self.speed
+
+        # resets the index if it is too big
+        if (self.index >= len(self.Images)):
+            self.index = 0
+            self.isAnimating = false
+
+    # display an image of the animation
+
+    def display(self, x, y, aSurface):
+        if (self.isAnimating):
+            imageIndex = int (self.index)
+            img = self.Images[imageIndex]
+            aSurface.blit(img, x, y)
+
+            #increment the index of the images to display
+            self.next()
+        else :
+            img = self.Images[0]
+            aSurface.blit(img, x, y)
